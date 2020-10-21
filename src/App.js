78ab23e36b1906426fd052770f0c85be0a9e1294
Moshiqu <912323520@qq.com>
2020-10-22 00:36:40 +0800

    添加scss和router的配置

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import About from "./views/About";
+import Home from "./views/Home";
+import News from "./views/News";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div >
+        <h1 >蒲晓雪是猪猪</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact component={Home} path="/" />
+            <Route component={About} path="/about" />
+            <Route component={News} path="/News" />
+          </Switch>
+        </BrowserRouter>
+      </div>
+
+      //   <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/views/News.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../aaa.scss";
+
+class News extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default News;
